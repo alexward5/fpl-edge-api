c64bf3fa6b9b0044a87711af2424dd9d95563e1c
alexward5 <alexanderward5@gmail.com>
2021-08-23 17:10:42 -0600

    update start message

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const resolvers = require("./resolvers");
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+  console.log(`Server listening at ${url}`);
 });
