c176fc4f04e7527cd7e8c0bc404f84a8266a07f8
alexward5 <alexanderward5@gmail.com>
2021-08-23 21:15:27 -0600

    implement player totals resolver

--- src/graph-ql/resolvers.ts
+++ src/graph-ql/resolvers.ts
@@ -1,14 +1,26 @@
 import pool from "../pg";
+import type Player from "../types/Player";
 
 const resolvers = {
   Query: {
-    players: async () => {
+    players: async (parent: undefined, { ids }: { ids: [number] }) => {
       const { rows } = await pool.query(`
         SELECT * FROM "2020-21".player_metadata
-        WHERE first_name = 'David'
+        ${ids ? `WHERE id IN (${ids.join(", ")})` : ""}
       `);
 
       return rows;
+    },
+  },
+  Player: {
+    player_totals: async (parent: Player) => {
+      const { rows } = await pool.query(`
+        SELECT *
+        FROM "2020-21".player_totals
+        WHERE id = ${parent.id}
+      `);
+
+      return rows[0];
     },
   },
 };
