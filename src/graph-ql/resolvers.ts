46418c9632cbbe0e1464d37d7701aeeec7bb93c4
alexward5 <alexanderward5@gmail.com>
2021-08-27 13:52:43 -0600

    improve error handling in resolvers

--- src/graph-ql/resolvers.ts
+++ src/graph-ql/resolvers.ts
@@ -4,23 +4,33 @@ import type Player from "../types/Player";
 const resolvers = {
   Query: {
     players: async (parent: undefined, { ids }: { ids: [number] }) => {
-      const { rows } = await pool.query(`
+      try {
+        const { rows } = await pool.query(`
         SELECT * FROM "2020-21".player_metadata
         ${ids ? `WHERE id IN (${ids.join(", ")})` : ""}
       `);
 
-      return rows;
+        return rows;
+      } catch ({ message }) {
+        console.log(`Error fetching players list: ${message}`);
+        return [];
+      }
     },
   },
   Player: {
     player_season_totals: async (parent: Player) => {
-      const { rows } = await pool.query(`
+      try {
+        const { rows } = await pool.query(`
         SELECT *
         FROM "2020-21".player_season_totals
         WHERE id = ${parent.id}
       `);
 
-      return rows[0];
+        return rows[0];
+      } catch ({ message }) {
+        console.log(`Error fetching player season totoals: ${message}`);
+        return {};
+      }
     },
     player_gameweek_data: async (
       parent: Player,
@@ -29,7 +39,8 @@ const resolvers = {
         gameweekEnd,
       }: { gameweekStart: number; gameweekEnd: number }
     ) => {
-      const { rows } = await pool.query(`
+      try {
+        const { rows } = await pool.query(`
         SELECT *
         FROM "2020-21".player_gameweek_data
         WHERE element = ${parent.id}
@@ -37,7 +48,11 @@ const resolvers = {
         ${gameweekEnd ? `AND round <= ${gameweekEnd}` : ""}
       `);
 
-      return rows;
+        return rows;
+      } catch ({ message }) {
+        console.log(`Error fetching player gameweek data: ${message}`);
+        return [];
+      }
     },
   },
 };
