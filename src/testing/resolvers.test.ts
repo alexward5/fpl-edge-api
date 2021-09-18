009b2c6c12e203b479bdf06405a4328e5492d480
alexward5 <alexanderward5@gmail.com>
2021-09-17 18:05:54 -0600

    implement test for player season totals

--- src/testing/resolvers.test.ts
+++ src/testing/resolvers.test.ts
@@ -29,6 +29,40 @@ const testCases = [
       ids: [1, 2, 3],
     },
   },
+  {
+    id: "getPlayerSeasonTotals",
+    query: `
+      query ($ids: [Int!]) {
+        players (ids: $ids) {
+          id
+          first_name
+          second_name
+          player_season_totals {
+            goals_scored
+            assists
+            total_points
+            minutes
+            goals_conceded
+            creativity
+            influence
+            threat
+            bonus
+            bps
+            ict_index
+            clean_sheets
+            red_cards
+            yellow_cards
+            selected_by_percent
+            now_cost
+            element_type
+          }
+        }
+      }
+    `,
+    variables: {
+      ids: [1, 2, 3],
+    },
+  },
 ];
 
-runTestCases("Players", testCases);
+runTestCases("PlayerData", testCases);
