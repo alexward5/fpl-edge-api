5ac5be5cd5e8263572668365de2ccd639382b9dc
Alex Ward <alexanderward5@gmail.com>
2024-11-11 18:34:24 -0700

    remove try/catch from resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,38 +1,28 @@
 import pool from "../pg";
 import type Player from "../types/Player";
 
-const SCHEMA = "2020-21";
+const SCHEMA = "test_schema";
 
 const resolvers = {
   Query: {
     players: async (parent: undefined, { ids }: { ids: [number] }) => {
-      try {
-        const { rows } = await pool.query(`
+      const { rows } = await pool.query(`
         SELECT * FROM "${SCHEMA}".player_metadata
         ${ids?.length ? `WHERE id IN (${ids.join(", ")})` : ""}
       `);
 
-        return rows;
-      } catch ({ message }) {
-        console.log(`Error fetching players list: ${message}`);
-        return [];
-      }
+      return rows;
     },
   },
   Player: {
     player_season_totals: async (parent: Player) => {
-      try {
-        const { rows } = await pool.query(`
+      const { rows } = await pool.query(`
         SELECT *
         FROM "${SCHEMA}".player_season_totals
         WHERE id = ${parent.id}
       `);
 
-        return rows[0];
-      } catch ({ message }) {
-        console.log(`Error fetching player season totoals: ${message}`);
-        return {};
-      }
+      return rows[0];
     },
     player_gameweek_data: async (
       parent: Player,
@@ -41,8 +31,7 @@ const resolvers = {
         gameweekEnd,
       }: { gameweekStart: number; gameweekEnd: number }
     ) => {
-      try {
-        const { rows } = await pool.query(`
+      const { rows } = await pool.query(`
         SELECT *
         FROM "${SCHEMA}".player_gameweek_data
         WHERE element = ${parent.id}
@@ -51,11 +40,7 @@ const resolvers = {
         ORDER BY round ASC
       `);
 
-        return rows;
-      } catch ({ message }) {
-        console.log(`Error fetching player gameweek data: ${message}`);
-        return [];
-      }
+      return rows;
     },
   },
 };
