7f804c4c6b3aff13c240dde9a45f0309ef9e0bc0
Alex Ward <alexanderward5@gmail.com>
2024-11-13 18:33:51 -0700

    create player data resolver

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -4,45 +4,35 @@ import type Player from "../types/Player";
 const SCHEMA = "test_schema";
 
 const resolvers = {
-  Query: {
-    players: async (parent: undefined, { ids }: { ids: [number] }) => {
-      const { rows } = await pool.query(`
-        SELECT * FROM "${SCHEMA}".player_metadata
-        ${ids?.length ? `WHERE id IN (${ids.join(", ")})` : ""}
-      `);
+    Query: {
+        players: async (parent: undefined) => {
+            const { rows } = await pool.query(`
+                SELECT * FROM "${SCHEMA}".v_player_data
+            `);
 
-      return rows;
+            return rows;
+        },
     },
-  },
-  Player: {
-    player_season_totals: async (parent: Player) => {
-      const { rows } = await pool.query(`
-        SELECT *
-        FROM "${SCHEMA}".player_season_totals
-        WHERE id = ${parent.id}
-      `);
+    Player: {
+        player_gameweek_data: async (
+            parent: Player,
+            {
+                gameweekStart,
+                gameweekEnd,
+            }: { gameweekStart: number; gameweekEnd: number }
+        ) => {
+            const { rows } = await pool.query(`
+                SELECT *
+                FROM "${SCHEMA}".player_gameweek_data
+                WHERE element = ${parent.id}
+                ${gameweekStart ? `AND round >= ${gameweekStart}` : ""}
+                ${gameweekEnd ? `AND round <= ${gameweekEnd}` : ""}
+                ORDER BY round ASC
+            `);
 
-      return rows[0];
+            return rows;
+        },
     },
-    player_gameweek_data: async (
-      parent: Player,
-      {
-        gameweekStart,
-        gameweekEnd,
-      }: { gameweekStart: number; gameweekEnd: number },
-    ) => {
-      const { rows } = await pool.query(`
-        SELECT *
-        FROM "${SCHEMA}".player_gameweek_data
-        WHERE element = ${parent.id}
-        ${gameweekStart ? `AND round >= ${gameweekStart}` : ""}
-        ${gameweekEnd ? `AND round <= ${gameweekEnd}` : ""}
-        ORDER BY round ASC
-      `);
-
-      return rows;
-    },
-  },
 };
 
 export default resolvers;
