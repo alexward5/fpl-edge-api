e68d45e5b2407a0d6bd11b44eb0a83146fa50311
Alex Ward <alexanderward5@gmail.com>
2025-01-28 17:38:13 -0700

    update player gameweek data type

--- src/types/PlayerGameweekData.ts
+++ src/types/PlayerGameweekData.ts
@@ -1,33 +1,8 @@
 interface PlayerGameweekData {
-  position: string;
-  xp: number;
-  assists: number;
-  bonus: number;
-  bps: number;
-  clean_sheets: number;
-  fixture: number;
-  goals_conceded: number;
-  goals_scored: number;
-  ict_index: number;
-  influence: number;
-  creativity: number;
-  threat: number;
-  kickoff_time: string;
-  minutes: number;
-  opponent_team: number;
-  own_goals: number;
-  penalties_missed: number;
-  saves: number;
-  penalties_saved: number;
-  yellow_cards: number;
-  red_cards: number;
-  round: number;
-  selected: number;
-  team_a_score: string;
-  team_h_score: string;
-  total_ponumbers: number;
-  value: number;
-  was_home: Boolean;
+    round: number;
+    fbref_npxg: number;
+    fbref_xg_assist: number;
+    calc_fpl_npxp: number;
 }
 
 export default PlayerGameweekData;
